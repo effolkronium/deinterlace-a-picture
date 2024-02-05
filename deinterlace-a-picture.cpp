b9b2aea2a66fabc624fe52f71cc677e687212158
Illia Polishchuk <effolkronium@gmail.com>
2024-02-05 04:44:42 -0800

    Add libjpeg deps

--- deinterlace-a-picture.cpp
+++ deinterlace-a-picture.cpp
@@ -2,14 +2,15 @@
 #include <filesystem>
 #include <iostream>
 #include <stdexcept>
+#include "jpeglib.h"
 
 namespace fs = std::filesystem;
-
-enum { e_bin_name, e_input_jpeg, e_output_jpeg, e_size_args };
 
 void deinterlace(const fs::path& input_jpeg, const fs::path& output_jpeg) {
     throw std::logic_error{"TODO"};
 }
+
+enum { e_bin_name, e_input_jpeg, e_output_jpeg, e_size_args };
 
 int main(int argc, char* argv[]) try {
     if (argc < e_size_args)
